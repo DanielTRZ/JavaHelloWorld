9eb12b5cd581dfb4655474523ceb6973a6ba2088
Daniel Trzeciński <71105883+DanielTRZ@users.noreply.github.com>
2024-07-07 17:48:27 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -69,7 +69,7 @@ String[] wiekUzytkownikow = new String[4];
 wiekUzytkownikow[0] = "Lat 25";
 wiekUzytkownikow[1] = "Lat 36";
 wiekUzytkownikow[2] = "Lat 40";
-		wiekUzytkownikow[3] = "Lat 56";
+wiekUzytkownikow[3] = "Lat 56";
 		System.out.println( "Wiek Użytkowników" + Arrays.toString(wiekUzytkownikow));
 		
 		 int liczba = 1; 
